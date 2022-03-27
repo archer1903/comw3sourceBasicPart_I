ba426ade635ea193e67acefeb0db8ebc37db16ff
archer1903 <98432117+archer1903@users.noreply.github.com>
2022-03-27 12:30:11 +0300

    Arrays E07 added.

--- w3ResoursceArrays/E7.java
+++ w3ResoursceArrays/E7.java
@@ -1,6 +1,24 @@
 package w3ResoursceArrays;
 
-public class E7 {
+import java.util.ArrayList;
 
+public class E7 {
+    public static void main(String[] args) {
+        /*
+        Write a Java program to remove a specific element from an array.*/
+
+        ArrayList<String> list=new ArrayList<>();
+
+        list.add("Ali");
+        list.add("Veli");
+        list.add("Deli");
+
+        System.out.println(list);
+        list.remove("Deli");
+        System.out.println(list);
+        list.remove(0);
+        System.out.println(list);
+
+    }
 
 }
